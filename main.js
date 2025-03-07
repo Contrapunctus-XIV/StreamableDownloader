536256fc7ad3dcb24a2da38341514a067846a608
ContrapunctusXIV <contrapunctusxiv@proton.me>
2025-03-07 15:08:11 +0100

    removed metadata from main

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         StreamableDownloader
-// @version      1.3.2
-// @namespace    streamable-dl
-// @description  Downloads the current Streamable video when you click the button.
-// @author       Contrapunctus-XIV
-// @match        *://streamable.com/*
-// @icon         https://cdn0.iconfinder.com/data/icons/ui-line-pixel-perfect-3/32/user_interface_UI_line_download_unduh_media_ui-512.png
-// @license      MIT
-// ==/UserScript==
-    
 const DELAY = 850;
     
 (function() {
